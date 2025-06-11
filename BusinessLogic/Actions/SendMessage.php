68e91572c98e65e93a37f3ab2248b7c7376ae24a
jake-schmm <jake.schmm@gmail.com>
2025-06-10 22:00:10 -0400

    Groundwork - Part 1

---
+++ BusinessLogic/Actions/SendMessage.php
@@ -0,0 +1,6 @@
+<?php 
+    // This will contain a method from MessageService to insert a new message
+
+    // Redirect back to ChatRoom page
+    header('Location: ChatRoom.php?SendingUserKey=1&RecipientUserKey=2')
+?>
