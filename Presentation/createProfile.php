4158c094fe0209db8960f826645d17fdeb4ea5cb
megdollar <megdollar@gmail.com>
2025-06-16 11:54:16 -0400

    clean up html

--- Presentation/createProfile.php
+++ Presentation/createProfile.php
@@ -4,27 +4,8 @@ include("head.php");
 include("header.php");
 ?>
 
-<html>
-<head>
-    <!-- Font Awesome -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-    />
-    <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
-    <!-- MDB -->
-    <link
-    href="https://cdn.jsdelivr.net/npm/mdb-ui-kit@9.0.0/css/mdb.min.css"
-    rel="stylesheet"
-    />
-   <link rel="stylesheet" href="../Assets/styles/ChatRoom.css">
-</head>
-<!---------------------------------------- TESTING --------------------------------------------------->
-<body>
+
+
 <main class="profile-container">
         <div class="profile-row">
             <!-- Left Column: Profile Picture and Delete Button -->
@@ -112,7 +93,6 @@ include("header.php");
             </div>
         </div>
     </main>
-<!------------------------------------------------------ TESTING ----------------------------------------------------------------------------------->
 <!---------------------- Modal --------------------------------->
                         <div id="customModal" class="modal">
                             <div class="modal-content">
