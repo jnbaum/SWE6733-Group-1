b12f969bae4ae7e0c3c423a4993b75275047be6e
Tarik Davis <Tdavi192@students.kennesaw.edu>
2025-06-23 14:38:01 -0400

    update to services

--- BusinessLogic/Services/UserService.php
+++ BusinessLogic/Services/UserService.php
@@ -115,31 +115,6 @@ class UserService {
             return false;
         }
     }
-}
-?><?php
-require_once(__DIR__ . "/../../DataAccess/DataAccess.php");
-require_once(__DIR__ . "/../../Models/UserDetails.php");
-require_once(__DIR__ . "/../../Models/MileRangeType.php");
-require_once(__DIR__ . "/../QueryHelper.php");
-
-
-class UserService {
-    private DataAccess $da;
-    public function __construct($da) {
-        $this->da = $da;
-    }
-
-    public function IsValidUser(string $enteredEmail, string $enteredPassword): ?int {
-        $query = "SELECT * FROM User WHERE Username=" . QueryHelper::SurroundWithQuotes($enteredEmail) . " LIMIT 1";
-        $stmt = $this->da->ExecuteQuery($query, QueryType::SELECT);
-        $row = $stmt->fetchAssociative(); 
-        // password_verify($plainTextPassword, $passwordHash) - returns true if the plain text password matches passwordHash after hash from password_hash() is applied
-        // https://www.phptutorial.net/php-tutorial/php-password_verify/
-        if($row && password_verify($enteredPassword, $row['PasswordHash'])) { // if a User was found in database with that username and entered password was valid
-            return $row['UserKey'];
-        }
-        return null;
-    }
 
     public function CreateNewUser(string $enteredEmail, string $enteredPassword): ?int {
         $query = "SELECT * FROM User WHERE Username=" . QueryHelper::SurroundWithQuotes($enteredEmail) . " LIMIT 1";
@@ -154,6 +129,6 @@ class UserService {
 
         return $insertedUserKey;
     }
-
 }
 ?>
+
