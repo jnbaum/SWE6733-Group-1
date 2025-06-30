e4d614fdd88c60a45874691f47742dabdbc44e97
Jerry Santiago <jerrysantiago128@gmail.com>
2025-06-30 07:23:52 -0400

    chore: creating test files

---
+++ tests/Unit/ProfileServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace something;
+
+use PHPUnit/Framework/TestCase;
+
+final class ProfileServiceTest extends TestCase{
+    $userKey= 1;
+    $fullName = "Test Name";
+    $bio = "this is my bio";
+    profileService->UpdateUserInfo()
+}
